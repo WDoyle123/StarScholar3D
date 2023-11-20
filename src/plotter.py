86396b2e74823d4a99dd92d309b97b00f6c42c7d
Will Doyle <will.doyle30@gmail.com>
2023-11-20 16:35:09 +0000

    create constellation.py and make big dipper, seems wrong so far

--- src/plotter.py
+++ src/plotter.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def plot_3d_scatter(x, y, z, rgb):
+def plot_3d_scatter(x, y, z, rgb, star_names=None):
     '''
     Creates a 3D plot using Cartesian coordinates with a black background,
     white points, grey axes, and black grid panes.
@@ -15,7 +15,26 @@ def plot_3d_scatter(x, y, z, rgb):
     ax.set_facecolor('black')
 
     # create scatter plot on the 3d projection
-    ax.scatter(x, y, z, color=rgb, s=15)
+    scatter = ax.scatter(x, y, z, color=rgb, s=15)
+
+    # add annotations if star names are provided
+    if star_names is not None:
+        for i in range(len(x)):
+            ax.text(x[i], y[i], z[i], star_names[i], color='white', fontsize=9)
+
+    if 'Phecda' in star_names and 'Megrez' in star_names:
+        star_names_list = list(star_names)
+        index_phecda = star_names_list.index('Phecda')
+        index_megrez = star_names_list.index('Megrez')
+
+        ax.plot([x[index_phecda], x[index_megrez]],
+                [y[index_phecda], y[index_megrez]],
+                [z[index_phecda], z[index_megrez]],
+                color='white', linewidth=1)
+
+    # Draw lines connecting the stars in the Big Dipper
+    # Ensure the stars are in the correct order for the Big Dipper
+    ax.plot(x, y, z, color='white', linewidth=1)
 
     # set labels and title with grey color
     ax.set_xlabel('X Coordinate', color='grey')
